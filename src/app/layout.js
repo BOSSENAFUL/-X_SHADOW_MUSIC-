3f77a7dfaaf1e5ade95fb4c1c0d177f2326a9c76
shreejaybhay <shreejaybhay26@gmail.com>
2026-02-28 07:43:01 +0530

    text select and zoom disable on phone screens

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,6 +68,13 @@ export const metadata = {
   manifest: "/manifest.json",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 
 export default function RootLayout({ children }) {
   return (
